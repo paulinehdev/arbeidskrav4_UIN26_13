9eaad55e79383c8393d6d55b7983f7f7d4ce0eef
FAE047 <faithengabou@hotmail.com>
2026-04-20 14:08:10 +0200

    Oprettet Sanity-schema for arbeidskrav og koblet det til schemaTypes/index.js

---
+++ backend/schemaTypes/arbeidskrav.js
@@ -0,0 +1,17 @@
+export default {
+    name: 'arbeidskrav',
+    title: 'Arbeidskrav',
+    type: 'document',
+    fields: [
+        {
+            name: 'title',
+            title: 'Tittel',
+            type: 'string', 
+        },
+        {
+            name: 'description', 
+            title: 'Kort beskrivelse',
+            type: 'text',
+        },
+    ],
+}
