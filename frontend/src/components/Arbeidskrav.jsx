9f31c00ffff33a333cb82a1291db4f887c071cda
FAE047 <faithengabou@hotmail.com>
2026-04-20 16:05:48 +0200

    Lagt til innhold i komponent og lagt til arbeidskrav.jsx

---
+++ frontend/src/components/Arbeidskrav.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import client from "../sanityClient";
+
+export default function Arbeidskrav(){
+    const [arbeidskrav, setArbeidskrav] = useState([])
+
+    useEffect(() => {
+        client.fetch(`*[_type == "arbeidskrav"]{title, description}`)
+        .then(data => setArbeidskrav(data));
+    }, []);
+
+    return (
+    <section>
+        <h2>Arbeidskrav</h2>
+        <ul>
+            {arbeidskrav.map((ak) => (
+                <li key={ak.title}>
+                    <h3>{ak.title}</h3>
+                    <p>{ak.description}</p>
+                </li>
+            ))}
+        </ul>
+    </section>
+    );
+}
