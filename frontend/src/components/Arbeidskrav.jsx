415b11f8857aadaaf211e3fcefbbd2a9caca6069
klpaulse <lovise_paulsen@hotmail.com>
2026-04-20 16:29:27 +0200

    endret sti vei til client

--- frontend/src/components/Arbeidskrav.jsx
+++ frontend/src/components/Arbeidskrav.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import client from "../sanityClient";
+import {client} from '../helpers/client.js'
+<source />
 
 export default function Arbeidskrav(){
     const [arbeidskrav, setArbeidskrav] = useState([])
