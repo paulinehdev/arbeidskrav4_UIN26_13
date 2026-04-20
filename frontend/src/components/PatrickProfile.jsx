e30ea5028739fa916f668673e2a1aa8f19fbd49e
PJ <Patrick_augjac@hotmail.com>
2026-04-20 15:50:02 +0200

    Fundament_på_plass
    Gjort ferdig jsx, lagd en css og lagt inn ett bilde av "meg"

--- frontend/src/components/PatrickProfile.jsx
+++ frontend/src/components/PatrickProfile.jsx
@@ -1,3 +1,19 @@
-export default function PatrickProfile(){
-    return
+import './PatrickProfile.css';
+import profilBilde from './Meg når jeg studerer.jpg';
+
+export default function PatrickProfile() {
+
+  return (
+    <article className="patrick-profile-kort">
+      <header>
+        <img 
+          src="Meg når jeg studerer.jpg" 
+          alt="Bildet av selveste patrick!" 
+        />
+        <h2>Patrick Jacobsen</h2>
+      </header>
+      <p>Bachelor i informasjonssystemer</p>
+      <a href="mailto:patricja@hiof.no">Send meg en mail!</a>
+    </article>
+  )
 }
