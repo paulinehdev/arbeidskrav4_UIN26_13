f6cbae888f73c36dfeb671c1b0da080cd9b4ff39
Pauline <pauline.haugerud10@gmail.com>
2026-04-20 16:54:57 +0200

    endret bildenavnet for å importere

--- frontend/src/components/PaulineProfile.jsx
+++ frontend/src/components/PaulineProfile.jsx
@@ -4,7 +4,7 @@ export default function PaulineProfile(){
     
     return (
         <article>
-            <img src="mitsi.jpg" alt="pauline-profilbilde" />
+            <img src={mitsi} alt="pauline-profilbilde" />
             <h2>Pauline Haugerud</h2>
             <p>Bachelor i Informasjonssystemer</p>
             <a href="mailto:paulineh@hiof.no">paulineh@hiof.no</a>
