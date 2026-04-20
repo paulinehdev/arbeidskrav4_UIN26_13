8771f19c321c0b27ac256bf536234ddfe197aece
Pauline <pauline.haugerud10@gmail.com>
2026-04-20 15:52:04 +0200

    laget profil-komponentet + importert bilde

--- frontend/src/components/PaulineProfile.jsx
+++ frontend/src/components/PaulineProfile.jsx
@@ -1,3 +1,13 @@
+import mitsi from '../assets/mitsi.jpg'
+
 export default function PaulineProfile(){
-    return
+    
+    return (
+        <article>
+            <img src="mitsi.jpg" alt="pauline-profilbilde" />
+            <h2>Pauline Haugerud</h2>
+            <p>Bachelor i Informasjonssystemer</p>
+            <a href="mailto:paulineh@hiof.no">paulineh@hiof.no</a>
+        </article>
+    )
 }
