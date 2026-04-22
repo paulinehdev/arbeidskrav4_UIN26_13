9c24185c23ef56f0640573e4a2165d520955a02e
klpaulse <lovise_paulsen@hotmail.com>
2026-04-22 16:09:33 +0200

    app.css lagt til og app.jsx endriner

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,16 +8,15 @@ import PaulineProfile from './components/PaulineProfile'
 function App() {
   
 
-  return (
+   return (
     <>
     <header>
       <h1>Gruppe 13</h1>
     </header>
 
     <main>
-      <section>
-        <h2>Gruppemedlemmer</h2>
-            
+      <h2>Gruppemedlemmer</h2>
+      <section className="medlem-kort">
         {/* Her skal vi lage komponentene våre */}
         <FaithProfile />
         <KarianneProfile />
